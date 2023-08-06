3cb8410fe1a769a577152dc9e07b802466532c4f
E-spacnvrnd <nasdaq80@mail.ru>
2023-08-06 10:26:10 +0700

    fix: update CORS allow all

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 mongoose.connect(DB_URL);
 
-app.use(cors({ origin: 'https://get-mesto.nomoreparties.co' }));
+app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
